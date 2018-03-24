7c8ab9f78ebc4e9f9efc04ba813837c7dfc5e7ed
Kyle <kyleturner49@knights.ucf.edu>
2018-03-23 20:13:12 -0400

    Add files via upload
    lol

--- Program2.c
+++ Program2.c
@@ -1,9 +1,4 @@
-// Group 14:	Kyle Turner
-//		Robert Shannahan
-//		John Millner
-// Program 2: 	Kernel Character Device
-
-// Includes
+//libraries.
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
@@ -11,102 +6,155 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 
-// Defines
-#define  DEVICE_NAME "CHAR_READER"	// The device will appear at /dev/CHAR_READER using this value
-#define  CLASS_NAME  "READER"		// The device class
+//static vals
+static int deviceNumber;
 
-// Global Variables
-static int    majorNumber;              // Stores the device number -- determined automatically
-static char   message[256] = {0};       // Memory for the string that is passed from userspace
-static short  size_of_message;          // Used to remember the size of the string stored
-static int    numberOpens = 0;          // Counts the number of times the device is opened
-static int    numberCloses = 0;		// Counts the number of times the device is closed
-static struct class*charClass  = NULL; 	// The device-driver class struct pointer
-static struct device*charDevice = NULL; // The device-driver device struct pointer
+static int numRead = 0;
+static int numOpen = 0;
+static int numClose = 0;
+static int numWrite = 0;
 
-// Structs
+int front = 0;
+int back = 0;
+int size = 0;
+
+static int error_count = 0;
+static char message[1024] = {0};
+static short sizeMessage;
+static struct class* group14Class = NULL;
+static struct device* group14Device = NULL;
+
+//prototype functions
+
+static int dev_open(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file*, const char *, size_t, loff_t *);
+static int dev_release(struct inode *, struct file *);
+
+//define
+#define BUFF_LEN 1024
+#define DEVICE_NAME "group14"
+#define CLASS_NAME "gp14"
+
+//MODULE_INFO
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Group 14");
+MODULE_DESCRIPTION("Linux Char Driver");
+MODULE_VERSION("1.0");
+
 static struct file_operations fops =
 {
-	.open = dev_open,
-	.close = dev_close,
-	.read = dev_read,
-	.write = dev_write,
-	.release = dev_release,
+    .open = dev_open,
+    .read = dev_read,
+    .write = dev_write,
+    .release = dev_release,
 };
 
-// Prototypes
-static int     dev_closes(struct inode *, struct file *);
-static int     dev_open(struct inode *, struct file *);
-static int     dev_release(struct inode *, struct file *);
-static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
-static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+   
 
-// Function to initialize the device
-static int __init char_init(void){
-	printk(KERN_INFO "Loading the device.\n");
+    static int __init group14_init(void){
+    //initialize
+    printk(KERN_INFO "group 14: Installing Module...\n");
+   
+    //make sure device number is > 0
+    deviceNumber = register_chrdev(0, DEVICE_NAME, &fops);
+        if(deviceNumber<0){
+        printk(KERN_ALERT "group 14 failed to register a positive number\n.");
+        return deviceNumber;
+        }
+    printk(KERN_INFO "group 14: registered correctly with number %d\n", deviceNumber);
+
+    group14Class = class_create(THIS_MODULE,CLASS_NAME);
+        if(IS_ERR(group14Class)){
+        unregister_chrdev(deviceNumber, DEVICE_NAME);
+        printk(KERN_ALERT "Failed to register device class\n");
+        return PTR_ERR(group14Class);
+        }
+   
+    group14Device = device_create(group14Class, NULL, MKDEV(deviceNumber, 0), NULL, DEVICE_NAME );
+        if(IS_ERR(group14Device)){
+        class_destroy(group14Class);
+        unregister_chrdev(deviceNumber, DEVICE_NAME);
+        printk(KERN_ALERT "Failed to create the device\n");
+        return PTR_ERR(group14Device);
+        }
+    printk(KERN_INFO "group 14: device class created correctly\n.");
+    return 0;
+    }
+
+    static void __exit group14_exit(void){
+    device_destroy(group14Class, MKDEV(deviceNumber, 0));
+    class_unregister(group14Class);
+    class_destroy(group14Class);
+    unregister_chrdev(deviceNumber, DEVICE_NAME);
+    printk(KERN_INFO "group 14: Goodbye!\n");
+    }
+       
+    static int dev_open(struct inode* inodep, struct file * filep){
+        numOpen++;
+    printk(KERN_INFO "group 14: Device has been opened %d time(s)\n", numOpen);
+    return 0;
+    }
+
+    static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
+ 
+    int i = 0;
+
+    // check for error
+    if(size >= BUFF_LEN){
+	printk(KERN_INFO "ERROR: Already Full.\n");
+	return -1;
+    }	
+
+    // report using printk each time it is written to.
+    numWrite++;
+    printk(KERN_INFO "group 14: Device has been written to %d time(s)\n", numWrite);
+
+    // add new string to buffer
+    for(i = 0; i < len; i++){
+	 // check for overflow
+   	 if(size >= BUFF_LEN){
+	 printk(KERN_INFO "ERROR: Already Full.\n");
+	 return -1;
+         }
+
+	message[back % BUFF_LEN] = buffer[i];
+
+	size++;
+	back = (back+1)%BUFF_LEN;
 	
-	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+    }// end for loop
 
-	// Check for valid major number
-	if(majorNumber < 0){
-		printk(KERN_INFO "ERROR: Could not assign major number.");
+    //if message is null then return 0
+    printk(KERN_INFO "group 14: Received %zu bytes from the user\n", len);
+    return len;
+    }
 
-		return majorNumber;
-	}
+    static ssize_t dev_read(struct file * filep, char * buffer, size_t len, loff_t *offset){
+    //report using printk each time it is written to 
+    numRead++;
+    printk(KERN_INFO "group 14: Device has been read from %d time(s)\n", numRead);
+    //read info
+    error_count = 0;
+ 
+    error_count = copy_to_user(buffer, message, sizeMessage);
+    
+        if (error_count==0) {
+       printk(KERN_INFO "Sent %d bytes to the user\n", sizeMessage);
+        return (sizeMessage=0);
+        }
+        else{
+        printk(KERN_INFO "group 14: Failure to send %d bytes to the user\n", error_count);
+        return -EFAULT;
+        }
+    }
 
-	// Print success message
-	printk(KERN_INFO "The device has been loaded successfully! The device number is: %d.\n", majorNumber);
+    static int dev_release(struct inode *inodep, struct file *filep){
+    numClose++;
+    printk(KERN_INFO "group 14: Device has been successfully closed %d time(s)\n", numClose);
+    return 0;
+    }
 
-	// Register the device
-	charDevice = deviceClass(charClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_Name);
+    module_init(group14_init);
+    module_exit(group14_exit);
 
-	// Clean up if there is an error
-	if(IS_ERR(charDevice)){
-		//
-		class_destroy(charClass);
-		unregister_chrdev(majorNumber, DEVICE_NAME);
-		printk(KERN_INFO "ERROR: Device could not be created\n");
-
-		return PTR_ERR(charDevice);
-	}
-
-	// Print success message
-	printk(KERN_INFO "The device class has been created successfully!\n");
-
-	return 0;
-}
-
-// Function to track how many times the device has been opened.
-static int dev_open(struct inode *inodep, struct file *filep){
-	numberOpens++;
-	printk(KERN_INFO "The device has been opened %d time(s)\n", numberOpens);
-	
-	return 0;
-}
-
-// Function to track how many times the device has been closed.
-static int dev_close(struct inode *inodep, struct file *filep){
-	numberCloses++;
-	printk(KERN_INFO "EBBChar: Device has been closed %d time(s)\n", numberCloses);
-	
-	return 0;
-}
-
-// Function to close the device.
-static int dev_release(struct inode *inodep, struct file *filep){
-	printk(KERN_INFO "The device has been closed.\n");
-
-	return 0;
-}
-
-// Function to exit.
-static int __exit chat_exit(void){
-	device_destroy(charClass, MKDEV(majorNumber, 0));
-   	class_unregister(charClass);
-   	class_destroy(charClass);
-   	unregister_chrdev(majorNumber, DEVICE_NAME);
-   	printk(KERN_INFO "The device says goodbye!\n");
-}
-
-module_init(char_init);
-module_exit(char_exit);
